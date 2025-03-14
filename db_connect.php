a20fcdcaea904674d95e48a75d7aa1babc97ee98
E3Coder <gotgothot.99@gmail.com>
2025-03-14 19:37:56 +0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,14 +1,14 @@
 <?php
-$servername = "158.108.101.153";
-$username = "std6630202252";
-$password = "M3@zWq7L";
-$dbname = "it_std6630202252";
+$servername = "IPserver"; // IP
+$username = "username"; // User
+$password = "password"; // Password
+$dbname = "db_name"; // Your database
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $conn->exec("SET NAMES 'utf8mb4'"); // บังคับ UTF-8
-    $conn->exec("SET CHARACTER SET utf8mb4"); // เพิ่มการตั้งค่าเพิ่มเติม
+    $conn->exec("SET NAMES 'utf8mb4'"); 
+    $conn->exec("SET CHARACTER SET utf8mb4"); 
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
     exit();
